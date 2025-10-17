a716715b2608c2c79e2a901859526c2bdbd2507a
Askar223 <askaribn223@gmail.com>
2025-10-18 01:06:09 +0600

    ask

--- Remove_Powered_by_Odoo/__manifest__.py
+++ Remove_Powered_by_Odoo/__manifest__.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Remove Powered by Odoo from Website Footer, Portal",
-    'summary': "Remove Powered by Odoo from Website Footer, Portal",
-    'description': "Remove Powered by Odoo from Website Footer, Portal",
     'version': '2.4',
+    'category': 'Website',
     'author': 'Real Tech',
     'price': 9.9,
     'currency': 'USD',
-    'category': 'Theme/Website',
     'license': 'LGPL-3',
-    'depends': ['website', 'portal'],
+    'summary': "Remove Powered by Odoo from Website Footer, Portal",
+    'depends': ['website'],
+    'category': 'Website',
     'data': [
         'views/templates.xml',
     ],
     'images': [
         'static/description/main_screenshot.gif',
-        'images/main_1.jpg',
-        'images/main_2.jpg',
-        'images/main_3.jpg'
+        'static/description/main_1.jpg',
+        'static/description/main_2.jpg',
+        'static/description/main_3.jpg'
     ],
     'installable': True,
     'application': False,
-    'auto_install': False,
 }
